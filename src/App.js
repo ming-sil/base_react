eac504e522ff9bbeea228eee8568c013898afe21
ming-sil <choming4926@gmail.com>
2022-06-14 13:06:32 +0900

    tag rule

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 const App = () => {
-  return <div>hello</div>;
+  return (
+    <>
+      <h1>h 태그!</h1>
+      <h2>h tag</h2>
+      <img src="#" alt="뿅" />
+    </>
+  );
 };
 
 export default App;
