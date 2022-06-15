08bf70c493097c5918da9732a386ac94c8650370
ming-sil <choming4926@gmail.com>
2022-06-15 09:57:23 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+// eslint-disable-next-line
 import TagRule from "./components/1_JsxRule/TagRule";
+// eslint-disable-next-line
 import ClassComponent from "./components/2_component/ClassComponent";
 // import FnComponent from "./components/2_component/FnComponent";
 import {
+  // eslint-disable-next-line
   FnComponent_1,
+  // eslint-disable-next-line
   FnComponent_2,
 } from "./components/2_component/FnComponent";
 import { FoodMenu } from "./components/2_component/FoodMenu";
@@ -20,7 +24,17 @@ const App = () => {
 
       {/* <ClassComponent /> */}
 
-      <FoodMenu />
+      {/* 3일차 */}
+      {/* *props? */}
+      {/* =><컴포넌트 프롭스명="값" /> */}
+      <h3>아침메뉴</h3>
+      <FoodMenu menuName="계란밥" beverage="커피" />
+
+      <h3>점심메뉴</h3>
+      <FoodMenu menuName="토마토리조또" beverage="콜라" />
+
+      <h3>저녁메뉴</h3>
+      <FoodMenu menuName="훠거" beverage="얼그레이 하이볼" />
     </>
   );
 };
--- src/components/2_component/FoodMenu.js
+++ src/components/2_component/FoodMenu.js
@@ -1,3 +1,15 @@
-export const FoodMenu = () => {
-  return <h3>오늘의 메뉴는 텐동</h3>;
+import "../../styles/style.css";
+
+export const FoodMenu = ({ menuName, beverage }) => {
+  // console.log(props);
+  // =>props는 오브젝트 형태로 가져오기때문에 매개변수에 중괄호를 적고 작성할 것
+  // console.log(menuName);
+
+  return (
+    <div>
+      오늘의 메뉴는 <i className="menu">{menuName}</i> 음료는
+      <i className="bev">{beverage}</i>
+      {/* =>jsx안쪽에 변수를 사용할 시 중괄호로 감싸줄 것 */}
+    </div>
+  );
 };
