e96a586a9adb1ffc4fc3d719a5dc4e9a958eea06
ming-sil <choming4926@gmail.com>
2022-06-20 10:38:47 +0900

    PlayWrap Style

--- src/components/5_MusicApp/components/PlayWrap.js
+++ src/components/5_MusicApp/components/PlayWrap.js
@@ -1,10 +1,28 @@
 import styled from "styled-components";
 
-const SPlayWrap = styled.div``;
+const SPlayWrap = styled.div`
+  width: 100%;
+  margin-top: 30px;
+`;
 
-const GageWrap = styled.div``;
+const GageWrap = styled.div`
+  height: 5px;
+  background-color: gray;
+`;
 
-const Gage = styled.div``;
+const Gage = styled.div`
+  width: 10%;
+  height: 100%;
+  background-color: mediumpurple;
+`;
+
+const TimeWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5px;
+  font-size: 12px;
+  font-weight: 100;
+`;
 
 export const PlayWrap = () => {
   return (
@@ -12,8 +30,10 @@ export const PlayWrap = () => {
       <GageWrap>
         <Gage />
       </GageWrap>
-      <span>00:03</span>
-      <span>03:40</span>
+      <TimeWrap>
+        <span>00:03</span>
+        <span>03:40</span>
+      </TimeWrap>
     </SPlayWrap>
   );
 };
