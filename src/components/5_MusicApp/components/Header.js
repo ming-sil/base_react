88b7838a7b4471b1ff3ba4b064da7caf36a00d3c
ming-sil <choming4926@gmail.com>
2022-06-20 10:18:40 +0900

    Header Style

--- src/components/5_MusicApp/components/Header.js
+++ src/components/5_MusicApp/components/Header.js
@@ -2,9 +2,27 @@ import { faBars, faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 
-const SHeader = styled.header``;
+const SHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+`;
 
-const Menus = styled.div``;
+const Menus = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:nth-child(1) {
+    background-color: white;
+    color: #1d1d1d;
+    font-size: 12px;
+  }
+  &:nth-child(2) {
+    border: 1px solid white;
+  }
+`;
 
 export const Header = () => {
   return (
