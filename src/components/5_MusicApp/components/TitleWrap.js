b35d7611123fc4eb60aad8dca1bcc1716f397aae
ming-sil <choming4926@gmail.com>
2022-06-20 10:31:11 +0900

    Title Style

--- src/components/5_MusicApp/components/TitleWrap.js
+++ src/components/5_MusicApp/components/TitleWrap.js
@@ -1,6 +1,18 @@
 import styled from "styled-components";
 
-const STitleWrap = styled.div``;
+const STitleWrap = styled.div`
+  text-align: center;
+  h3 {
+    font-size: 22px;
+    font-weight: 700;
+    margin-bottom: 5px;
+  }
+  p {
+    font-size: 14px;
+    font-weight: 100;
+    opacity: 0.7;
+  }
+`;
 
 export const TitleWrap = () => {
   return (
