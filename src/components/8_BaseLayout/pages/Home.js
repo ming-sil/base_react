1fc2815f4cce4e4e955eee8a45a2dc693d215bfd
ming-sil <choming4926@gmail.com>
2022-06-22 11:12:46 +0900

    sub page

--- src/components/8_BaseLayout/pages/Home.js
+++ src/components/8_BaseLayout/pages/Home.js
@@ -1,3 +1,46 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Banner = styled.section`
+  height: 800px;
+  background-color: lightgray;
+`;
+
+const Section = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 120px 400px;
+`;
+
+const Content = styled.div`
+  width: 45%;
+  height: 500px;
+  background-color: lightgray;
+  box-shadow: 15px 15px 20px rgba(0, 0, 0, 0.3);
+  transition: 0.5s;
+  &:hover {
+    transform: translate(15px, 15px);
+    box-shadow: none;
+  }
+  a {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
 export const Home = () => {
-  return <>Home</>;
+  return (
+    <>
+      <Banner></Banner>
+      <Section>
+        <Content>
+          <Link to="/sub1"></Link>
+        </Content>
+        <Content>
+          <Link to="/sub2"></Link>
+        </Content>
+      </Section>
+    </>
+  );
 };
